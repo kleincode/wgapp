a3569db24f06e35c6001d25efd4e610352ada915
TR10Programming <f.kleinst10810@gmail.com>
2020-04-01 12:06:51 +0200

    Add household reimplemented

--- client/src/views/household/AddHousehold.vue
+++ client/src/views/household/AddHousehold.vue
@@ -1,47 +1,90 @@
 <template>
-  <div>
-    <div class="md-layout md-gutter md-alignment-top-center">
-      <md-card class="view-card">
-        <md-card-header>
-          <div class="md-title">Add household</div>
-        </md-card-header>
-        <md-card-content>
-          <form @submit.prevent="formSubmit" :auto-complete="false">
-            <md-field>
-              <label>Household ID</label>
-              <md-input
-                v-model="householdId"
-                required
-                :maxlength="32"
-              ></md-input>
-            </md-field>
-            <md-field>
-              <label>Security Code</label>
-              <md-input
-                v-model="securityCode"
-                required
-                :maxlength="6"
-              ></md-input>
-            </md-field>
-            <md-button class="md-raised md-primary" type="submit">
-              Check
-            </md-button>
-          </form>
-        </md-card-content>
-      </md-card>
-    </div>
+  <div class="md-layout md-gutter md-alignment-center-center fill-view">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Join household</v-toolbar-title>
+            </v-toolbar>
+            <v-form @submit.prevent="formSubmit" ref="form">
+              <v-card-text>
+                <v-text-field
+                  label="Household ID"
+                  v-model="householdId"
+                  prepend-icon="home"
+                  type="text"
+                  outlined
+                  :rules="validating ? householdIdRules : []"
+                />
+                <v-text-field
+                  label="Security code"
+                  v-model="securityCode"
+                  prepend-icon="vpn_key"
+                  type="text"
+                  outlined
+                  :rules="validating ? securityCodeRules : []"
+                />
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" type="submit" :loading="loading">
+                  <v-icon left>arrow_forward</v-icon>
+                  Move in
+                </v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-snackbar :timeout="4000" v-model="showSnackbar">
+      <span>{{ snackbarMessage }}</span>
+      <v-btn text small color="red" @click="showSnackbar = false">Close</v-btn>
+    </v-snackbar>
   </div>
 </template>
+
 <script>
 export default {
   name: "AddHousehold",
   data: () => ({
     householdId: "",
-    securityCode: ""
+    securityCode: "",
+    loading: false,
+    showSnackbar: false,
+    snackbarMessage: "",
+    householdIdRules: [
+      v => !!v || 'Household ID is required',
+      v => (v && v.length == 32 && /^[A-Z0-9]*$/.test(v)) || 'Invalid format'
+    ],
+    securityCodeRules: [
+      v => !!v || 'Security code is required',
+      v => (v && v.length == 6 && /^[0-9]*$/.test(v)) || 'Invalid format'
+    ],
+    validating: false,
+    formValid: null
   }),
   methods: {
-    formSubmit() {
-      alert("form submitted");
+    alertSnackbar(msg) {
+      if (this.showSnackbar) {
+        this.showSnackbar = false;
+        setTimeout(() => this.alertSnackbar(msg), 500);
+      } else {
+        this.snackbarMessage = msg;
+        this.showSnackbar = true;
+      }
+    },
+    async validate() {
+      this.validating = true;
+      this.$refs.form.validate();
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      this.formValid = this.$refs.form.validate();
+    },
+    async formSubmit() {
+      await this.validate();
+      if(!this.formValid) this.alertSnackbar("Please check your input.");
+      else this.alertSnackbar("form submitted");
     }
   },
   mounted() {
@@ -50,10 +93,3 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-.view-card {
-  min-width: 50%;
-  max-width: 90%;
-  width: 700px;
-}
-</style>
