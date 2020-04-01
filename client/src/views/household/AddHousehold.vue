73ee2189107fa3d84bc138d342f9f6b1443632ff
TR10Programming <f.kleinst10810@gmail.com>
2020-04-01 13:26:14 +0200

    Create household reimplementation

--- client/src/views/household/AddHousehold.vue
+++ client/src/views/household/AddHousehold.vue
@@ -2,7 +2,7 @@
   <div class="md-layout md-gutter md-alignment-center-center fill-view">
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
-        <v-col cols="12" sm="8" md="4">
+        <v-col cols="12" sm="8" md="6" xl="4">
           <v-card class="elevation-12">
             <v-toolbar color="primary" dark flat>
               <v-toolbar-title>Join household</v-toolbar-title>
