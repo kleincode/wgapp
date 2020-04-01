3eaa541b138c88c9ae23ce9b8b2537dbc84b3fae
TR10Programming <f.kleinst10810@gmail.com>
2020-04-01 19:07:53 +0200

    Added axios, reimplemented existing AJAX requests

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,18 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+//Vuetify
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
+//Axios
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
