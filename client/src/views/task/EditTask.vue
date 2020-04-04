e2eabcd617c4a5ea8b2062c90b58bffde4d0e4f9
Matti Frind <matti@frind.de>
2020-04-04 23:54:29 +0200

    minor updates

--- client/src/views/task/EditTask.vue
+++ client/src/views/task/EditTask.vue
@@ -1,13 +1,7 @@
 <template>
   <v-container fluid>
     <div style="display: flex">
-      <v-btn
-        exact
-        icon
-        color="primary"
-        :to="{ name: 'Tasks' }"
-        class="mt-2 mr-2"
-      >
+      <v-btn exact icon color="primary" @click="back" class="mt-2 mr-2">
         <v-icon>keyboard_arrow_left</v-icon>
       </v-btn>
       <div v-if="editMode" class="display-2 pb-6">Edit task - "{{ name }}"</div>
@@ -15,140 +9,166 @@
     </div>
     <v-card outlined>
       <div class="container">
-        <v-row>
-          <v-col cols="12" md="8" lg="8">
-            <v-text-field
-              v-model="name"
-              :counter="128"
-              label="Name"
-              required
-              outlined
-            ></v-text-field>
-            <div class="title">Member mode</div>
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-radio-group v-model="iterating">
-                  <v-radio :key="0" :label="'Single'" :value="false"></v-radio>
-                  <v-radio
-                    :key="1"
-                    :label="'Iterating'"
-                    :value="true"
-                  ></v-radio>
-                </v-radio-group>
-              </v-col>
-              <v-col cols="12" md="4">
-                <v-select
-                  :items="assignedMember"
-                  label="Assigned to"
-                  outlined
-                  :disabled="iterating"
-                ></v-select>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="12" md="4" lg="4" style="text-align: center">
-            <v-icon style="font-size: 12em">{{
-              this.getIcon(this.icon)
-            }}</v-icon>
-            <br />
-            <IconChooser v-model="icon"></IconChooser>
-          </v-col>
-        </v-row>
-        <div class="title">Time & Date</div>
-        <v-row>
-          <v-col cols="12" lg="4" md="6">
-            <v-menu
-              v-model="startDateMenu"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" md="8" lg="8">
+              <v-text-field
+                v-model="name"
+                :counter="128"
+                label="Name"
+                required
+                outlined
+              ></v-text-field>
+              <div class="title">Member mode</div>
+              <v-row>
+                <v-col cols="12" md="6">
+                  <v-radio-group v-model="iterating">
+                    <v-radio
+                      :key="0"
+                      :label="'Single'"
+                      :value="false"
+                    ></v-radio>
+                    <v-radio
+                      :key="1"
+                      :label="'Iterating'"
+                      :value="true"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-select
+                    :items="assignedMember"
+                    label="Assigned to"
+                    outlined
+                    :disabled="iterating"
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="4" lg="4" style="text-align: center">
+              <v-icon style="font-size: 12em">{{
+                this.getIcon(this.icon)
+              }}</v-icon>
+              <br />
+              <IconChooser v-model="icon"></IconChooser>
+            </v-col>
+          </v-row>
+          <div class="title">Time & Date</div>
+          <v-row>
+            <v-col cols="12" lg="4" md="6">
+              <v-menu
+                v-model="startDateMenu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="date"
+                    label="Choose your start day"
+                    prepend-icon="event"
+                    readonly
+                    v-on="on"
+                    outlined
+                  ></v-text-field>
+                </template>
+                <v-date-picker
                   v-model="date"
-                  label="Choose your start day"
-                  prepend-icon="event"
-                  readonly
-                  v-on="on"
-                  outlined
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="date"
-                @input="startDateMenu = false"
-              ></v-date-picker>
-            </v-menu>
-          </v-col>
-          <v-col cols="12" lg="4" md="6">
-            <v-menu
-              ref="menu"
-              v-model="startTimeMenu"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              :return-value.sync="time"
-              transition="scale-transition"
-              offset-y
-              max-width="290px"
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
+                  @input="startDateMenu = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="12" lg="4" md="6">
+              <v-menu
+                ref="menu"
+                v-model="startTimeMenu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="time"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="time"
+                    label="Choose you task time"
+                    prepend-icon="access_time"
+                    readonly
+                    v-on="on"
+                    outlined
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="startTimeMenu"
                   v-model="time"
-                  label="Choose you task time"
-                  prepend-icon="access_time"
-                  readonly
-                  v-on="on"
-                  outlined
-                ></v-text-field>
-              </template>
-              <v-time-picker
-                v-if="startTimeMenu"
-                v-model="time"
-                format="24hr"
-                full-width
-                @click:minute="$refs.menu.save(time)"
-              ></v-time-picker>
-            </v-menu>
-          </v-col>
-          <v-col cols="12" lg="4" md="6">
-            <v-switch v-model="reminder" label="Toggle Reminder"></v-switch>
-          </v-col>
-          <v-col cols="12" lg="4" md="6">
-            <v-select
-              v-model="chosenDays"
-              :items="days"
-              chips
-              label="Repeat on"
-              multiple
-              outlined
-            ></v-select>
-          </v-col>
-          <v-col cols="12" lg="4" md="6">
-            <v-text-field
-              type="number"
-              v-model="repetitionEvery"
-              label="Repeat every"
-              required
-              outlined
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12" lg="4" md="6">
-            <v-select
-              v-model="repetitionUnit"
-              :items="repetitionUnits"
-              label="Repetition unit"
-              outlined
-            ></v-select>
-          </v-col>
-          <v-col cols="12" style="text-align: right">
-            <v-btn large color="primary" @click="postChanges()" class="mr-4"
-              >save</v-btn
-            >
-            <v-btn large :to="{ name: 'Tasks' }">cancel</v-btn>
-          </v-col>
-        </v-row>
+                  format="24hr"
+                  full-width
+                  @click:minute="$refs.menu.save(time)"
+                ></v-time-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="12" lg="4" md="6">
+              <v-switch v-model="reminder" label="Toggle Reminder"></v-switch>
+            </v-col>
+            <v-col cols="12" lg="4" md="6">
+              <v-select
+                v-model="chosenDays"
+                :items="days"
+                chips
+                label="Repeat on"
+                multiple
+                outlined
+              ></v-select>
+            </v-col>
+            <v-col cols="12" lg="4" md="6">
+              <v-text-field
+                type="number"
+                v-model="repetitionEvery"
+                label="Repeat every"
+                required
+                outlined
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" lg="4" md="6">
+              <v-select
+                v-model="repetitionUnit"
+                :items="repetitionUnits"
+                label="Repetition unit"
+                outlined
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions style="text-align: right; display: block">
+          <v-btn large color="primary" @click="postChanges()">save</v-btn>
+          <v-btn large :to="{ name: 'Tasks' }">cancel</v-btn
+          ><v-divider class="mx-4" inset vertical></v-divider>
+          <v-dialog v-model="deleteDialog" width="500">
+            <template v-slot:activator="{ on }">
+              <v-btn large v-on="on" outlined color="error darken-2"
+                >delete</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title class="headline" primary-title>
+                Do you really want to delete this task?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error" text @click="deleteTask">
+                  delete
+                </v-btn>
+                <v-btn text @click="deleteDialog = false">
+                  cancel
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card-actions>
       </div>
     </v-card>
     <v-snackbar v-model="snackbar">
@@ -169,7 +189,7 @@ export default {
   data: () => ({
     editMode: false,
     id: -1,
-    name: "Bad putzen",
+    name: "",
     iterating: true,
     icon: 0,
     selectedMember: "none",
@@ -183,18 +203,19 @@ export default {
       "Saturday",
       "Sunday"
     ],
-    chosenDays: ["Friday"],
+    chosenDays: [],
     time: null,
     reminder: false,
     repetitionEvery: "",
     repetitionUnit: "",
     repetitionUnits: ["Weeks", "Months"],
-    assignedMember: ["Felix", "Matti", "Tom"],
+    assignedMember: [""],
 
     startDateMenu: false,
     startTimeMenu: false,
     snackbar: false,
-    snackText: ""
+    snackText: "",
+    deleteDialog: false
   }),
   methods: {
     async fetch_settings(id) {
@@ -221,7 +242,6 @@ export default {
         this.repetitionEvery = parseInt(task.repetitionEvery);
         this.repetitionUnit = this.repetitionUnits[task.repetitionUnit];
         this.reminder = !!task.reminder;
-        console.log(this.repetitionUnit);
       }
     },
 
@@ -230,13 +250,11 @@ export default {
       let name = this.name;
       let icon = this.icon;
       let iterating;
-      console.log("iter: " + this.iterating);
       if (this.iterating) {
         iterating = 1;
       } else {
         iterating = 0;
       }
-      console.log("iter: " + iterating);
       let selectedMember = 8; //TODO this.assignedMember
       let chosenDays = this.chosenDays.map(
         day => day[0].toLowerCase() + day.substr(1, day.length)
@@ -262,23 +280,50 @@ export default {
         return;
       }
 
-      await this.$http.post("/_/edittask", {
-        id,
-        name,
-        icon,
-        iterating,
-        selectedMember,
-        chosenDays,
-        time,
-        repetitionEvery,
-        repetitionUnit,
-        reminder,
-        startDate
-      });
+      if (this.editMode) {
+        await this.$http.post("/_/edittask", {
+          id,
+          name,
+          icon,
+          iterating,
+          selectedMember,
+          chosenDays,
+          time,
+          repetitionEvery,
+          repetitionUnit,
+          reminder,
+          startDate
+        });
+      } else {
+        let assignedMember = selectedMember;
+        let date = startDate;
+        const { data } = await this.$http.post("/_/addtask", {
+          id,
+          name,
+          icon,
+          iterating,
+          assignedMember,
+          repetitionEvery,
+          repetitionUnit,
+          reminder,
+          date,
+          time
+        });
+        console.log(data);
+      }
       this.$router.push({ name: "Tasks" });
     },
     getIcon(id) {
       return icons[id];
+    },
+    deleteTask() {
+      this.deleteDialog = false;
+      //TODO
+      this.$router.push({ name: "Tasks" });
+    },
+
+    back() {
+      this.$router.back();
     }
   },
   mounted() {
