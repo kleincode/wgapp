844cfb548c4d45bce269e6f8c43b40f9196ce7c5
erikh8 <erik.8h@live.de>
2020-04-27 12:13:52 +0200

    merge rebase

---
+++ client/src/views/help/FinancesHelp.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <h2 class="mb-4">
+      Finances
+    </h2>
+    <p class="text-justify body-1">
+      While living in a shared apartment, there are always expenses that benefit
+      everyone. In order not to lose the overview, you can enter all these
+      expenses under "Finances".
+    </p>
+    <p class="text-justify body-1">
+      If you have bought something like this, click on "New Expence" and enter
+      your expenditure. To show full transparency you can also upload the
+      receipt here. If you don't feel like taking a picture of the receipt at
+      the moment or to clear faults, you can add the bill later or edit your
+      Entry under "Actions".
+    </p>
+    <p class="text-justify body-1">
+      GIF: "Upload a receipt"
+    </p>
+    <p class="text-justify body-1">
+      At the top right you can choose a time span over which the expances should
+      be displayed. This setting affects beside the "Member Expances" also the
+      overview and the trendcurve below.
+    </p>
+    <p class="text-justify body-1"></p>
+    <p class="text-justify body-1"></p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "FinancesHelp",
+  data: () => ({
+    var1: 2,
+    var2: 1,
+    var3: 0.5
+  })
+};
+</script>
---
+++ client/src/views/help/OverviewHelp.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <h2 class="mb-4">
+      Overview
+    </h2>
+    <p class="text-justify body-1">
+      You need help? We are there for you! Take a look.
+    </p>
+    <v-row justify="space-around">
+      <v-col cols="3">
+        <div class="title mb-1">GIF sample</div>
+        <div class="subheading">aspect-ration: 2</div>
+        <v-img
+          src="https://gamespot1.cbsistatic.com/uploads/original/1551/15516546/2943956-7961897803-littl.gif"
+          aspect-ratio="2"
+        ></v-img>
+      </v-col>
+      <v-col cols="3">
+        <div class="title mb-1">GIF sample</div>
+        <div class="subheading">aspect-ration: 1</div>
+        <v-img
+          src="https://gamespot1.cbsistatic.com/uploads/original/1551/15516546/2943956-7961897803-littl.gif"
+          aspect-ratio="1"
+        ></v-img>
+      </v-col>
+      <v-col cols="3">
+        <div class="title mb-1">GIF sample</div>
+        <div class="subheading">aspect-ration: 0.5</div>
+        <v-img
+          src="https://gamespot1.cbsistatic.com/uploads/original/1551/15516546/2943956-7961897803-littl.gif"
+          aspect-ratio="0.5"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-card class="warning"><v-card-text>text</v-card-text> </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "OverviewHelp"
+};
+</script>
