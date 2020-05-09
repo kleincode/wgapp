6b67b2a0f31f65ab36b115e78968f2af02e4824d
Matti Frind <matti@frind.de>
2020-05-09 12:10:55 +0200

    add vuetify and axios

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
+import "./plugins/axios";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
