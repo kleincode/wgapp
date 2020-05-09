f6e73917f9ce755a173d23133bfc6727c6528a2d
Matti Frind <matti@frind.de>
2020-05-09 12:39:00 +0200

    initial design, theme added

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -6,47 +6,28 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="@/assets/jeff-without.svg"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <div class="headline">Jeff - Admin Panel</div>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld />
+      <Home />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Home from "./views/Home.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Home
   },
 
   data: () => ({
--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <h1 class="display-2 mt-12 mb-6">Statistics</h1>
+    <v-row justify="center">
+      <v-col cols="12" md="4" class="text-center mb-2">
+        <v-card :elevation="7">
+          <v-card-content class="pa-8">
+            <v-row justify="center">
+              <v-col cols="12" md="2">
+                <v-icon color="black" style="font-size: 8em">person</v-icon>
+              </v-col>
+              <v-col cols="12" md="5">
+                <div class="display-4 mb-3">24</div>
+                <div class="overline" style="font-size: 1em !important">
+                  Users
+                </div>
+              </v-col>
+            </v-row>
+          </v-card-content>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4" class="text-center">
+        <v-card :elevation="7">
+          <v-card-content class="pa-8">
+            <v-row justify="center">
+              <v-col cols="12" md="2">
+                <v-icon color="black" style="font-size: 8em">home</v-icon>
+              </v-col>
+              <v-col cols="12" md="5">
+                <div class="display-4 mb-3">24</div>
+                <div class="overline" style="font-size: 1em !important">
+                  Households
+                </div>
+              </v-col>
+            </v-row>
+          </v-card-content>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-card class="mt-8">
+      <v-card-title class="display-2">Log</v-card-title>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
