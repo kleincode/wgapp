fbc1f81a273e4127baf504a0d6c68354785f84d0
TR10Programming <f.kleinst10810@gmail.com>
2020-05-10 16:50:34 +0200

    Send verify email

--- server/handlers/register.js
+++ server/handlers/register.js
@@ -1,6 +1,11 @@
-/*  Handler for "/_/register". Purpose: Adding new user to database. */
+/*  Handler for "/_/register". Purpose: Adding new user to database and sending an verification email. */
 const BCrypt = require("bcryptjs");
 const AUTH_SALT_ROUNDS = parseInt(process.env.AUTH_SALT_ROUNDS) || 10;
+const { v4: uuidv4 } = require("uuid");
+const JWT = require("jsonwebtoken");
+const JWT_SECRET = process.env.FORGOT_PASS_SECRET || "jIiTU_#=i9:S`Deh";
+const mailtext = require("../mails/verifyemail");
+const transporter = require("../components/Mailer");
 
 module.exports = ({ db }) => ({
   type: "POST",
@@ -29,28 +34,57 @@ module.exports = ({ db }) => ({
   handler: async ({ body, query }, { success, fail, error }) => {
     const { email, firstname, lastname, password } = body;
     if(email.match(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)) {
+      const ta = await db.beginTransaction();
       try {
-        const { results: userResults } = await db.query(`SELECT COUNT(*) AS c FROM users WHERE ?`, {email: email});
+        const { results: userResults } = await ta.query(`SELECT COUNT(*) AS c FROM users WHERE ?`, {email: email});
 
         if(userResults[0].c > 0) {
+          await ta.rollback();
           fail("This email address is already registered.");
         } else try {
           //Actual register process begins here
           const hash = await BCrypt.hash(password, AUTH_SALT_ROUNDS);
 
           try {
-            await db.query(
+            const { results: { insertId: uid } } = await ta.query(
               "INSERT INTO `users` (`email`, `firstname`, `lastname`, `pass`) VALUES (?, ?, ?, ?)",
               [email, firstname, lastname, hash]
             );
-            success(`You were successfully registered, ${firstname}.`);
+            if(!uid) throw `UID of inserted user is falsy (${uid})`;
+            // Email verification
+            try {
+              // Generate and insert verification token
+              const cpwtoken = uuidv4();
+              await ta.query("INSERT INTO usertokens (`uid`, `type`, `token`) VALUES (?, ?, ?)", [ uid, "verify", cpwtoken]);
+              // Create and send payload to e-mail address
+              const token = JWT.sign({ email, cpwtoken }, JWT_SECRET, { expiresIn: "3d" });
+              try {
+                await transporter.sendMail({
+                  from: '"Jeff" <jeff@kleinco.de>', // sender address
+                  to: "f.kleinst10810@gmail.com" || email, // list of receivers
+                  subject: "Verify your email", // Subject line
+                  html: mailtext({ firstname, lastname, email, token })
+                });
+                await ta.commit();
+                success(`You were successfully registered, ${firstname}. Please check your mail box.`);
+              } catch(err) {
+                await ta.rollback();
+                error("Could not send verification email. Please check your email address.", err);
+              }
+            } catch(err) {
+              await ta.rollback();
+              error("Error while creating verification token. Registration process cancelled.", err);
+            }
           } catch(err) {
+            await ta.rollback();
             error("The registration process failed.", err);
           }
         } catch(err) {
+          await ta.rollback();
           error("Error during encryption process.", err);
         }
       } catch(err) {
+        await ta.rollback();
         error("Error while fetching users from database.", err);
       }
     } else {
---
+++ server/mails/verifyemail.js
@@ -0,0 +1,28 @@
+const baseUrl = process.env.SERVER_BASE_URL || "http://localhost";
+
+module.exports = ({ firstname, lastname, email, token }) => `
+
+Hi ${firstname},
+<p>
+    welcome on board! Jeff is the greatest choice you can make for your personal home management needs
+    - and I'm not just saying that because I am Jeff but also because it's true. At least that's what
+    my mother says. Anyways, please clicky clicky on that linky linky so I know you're for real:
+</p>
+<p>
+    <a href="${baseUrl}/login/?verify=${encodeURIComponent(token)}" target="_blank">Verify email address</a><br />
+</p>
+<p>
+    In case you didn't register, shame on you. You really should. The two of us would make a great team.
+    Oh, and incidentally, some troll is entering your mail address on dubious (yet totally likeable) online apps.
+    You should really *address* this issue to them.
+</p>
+<br />
+<p>
+    See you on the other tab,<br />
+    Jeff.
+</p>
+<p>
+    PS: Humor update is scheduled for next month. Stay tuned.
+</p>
+
+`;
