7694f96b56449d822bbb4b53cc2e75999c232e78
Matti Frind <matti@frind.de>
2020-05-28 12:29:50 +0200

    Small Task Display redesigned

--- client/src/components/tasks/SmallTaskDisplay.vue
+++ client/src/components/tasks/SmallTaskDisplay.vue
@@ -1,27 +1,32 @@
 <template>
-  <div style="height: 70%; width:100%">
+  <div style=" width: 100%;">
     <v-card
       v-if="!!task"
-      class="small-main-task text-center"
-      :class="cardColor"
+      class="mt-2 mb-2"
+      :class="$vuetify.theme.isDark ? 'secondary' : 'white'"
       :elevation="6"
-      style="height: 100%"
+      style="height: 100%;"
       :style="'color: ' + textColor"
       :dark="!$vuetify.theme.isDark"
     >
-      <v-row align="stretch" style="height: 100%">
-        <v-col cols="12" class="overline pt-0 pl-0 pr-0 pb-1">
-          {{ taskMode }}
+      <v-row align="center" class="ma-0">
+        <v-col cols="2">
+          <v-icon
+            class="pa-0 ma-0"
+            :style="'font-size: ' + iconsize"
+            :color="textColor"
+            x-large
+            >{{ getIcon(task.icon) }}</v-icon
+          >
         </v-col>
-        <v-col cols="3" lg="2" class="pt-0 pb-0">
-          <v-icon style="font-size: 3em" :color="textColor" x-large>{{
-            getIcon(task.icon)
-          }}</v-icon>
-        </v-col>
-        <v-col cols="6" md="6" lg="7" class="text-left title pt-0 pb-0">
-          {{ task.name }}
-          <br />
-          <v-chip>
+        <v-col cols="8" class="text-left">
+          <center class="overline">{{ taskMode }}</center>
+          <div
+            style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
+          >
+            {{ task.name }}
+          </div>
+          <v-chip small>
             <v-avatar :color="!userImages[task.assigned] ? 'primary' : ''" left>
               <v-img
                 v-show="userImages[task.assigned]"
@@ -33,21 +38,46 @@
             </v-avatar>
             {{ getUserName(task.assigned) }}
           </v-chip>
+          <v-btn
+            v-if="$vuetify.breakpoint.mdAndUp"
+            :color="textColor"
+            icon
+            @click="$emit('triggerreminder', task)"
+          >
+            <v-icon>notifications_active</v-icon>
+          </v-btn>
         </v-col>
-        <v-col class="pt-0 pb-0" cols="3" md="2" lg="3">
-          <div class="font-regular display-1">
-            <v-btn :color="textColor" icon @click="$emit('checktask', task)">
-              <v-icon v-if="task.checked">check_box</v-icon>
-              <v-icon v-else>check_box_outline_blank</v-icon>
-            </v-btn>
-            <v-btn
-              :color="textColor"
-              icon
-              @click="$emit('triggerreminder', task)"
-            >
-              <v-icon>notifications_active</v-icon>
-            </v-btn>
-          </div>
+        <v-col cols="2" class="pa-0 ma-0">
+          <v-sheet
+            :color="cardColor"
+            style="border-radius: 0px; height:100px; margin-right: -1px !important"
+          >
+            <v-row style="height: 100%" justify="center" align="center">
+              <v-col cols="12" class="pa-0">
+                <v-btn
+                  :color="textColor"
+                  icon
+                  @click="$emit('checktask', task)"
+                >
+                  <v-icon v-if="task.checked">check_box</v-icon>
+                  <v-icon v-else>check_box_outline_blank</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col
+                v-if="$vuetify.breakpoint.smAndDown"
+                cols="12"
+                class="pa-0"
+              >
+                <v-btn
+                  :color="textColor"
+                  icon
+                  @click="$emit('triggerreminder', task)"
+                >
+                  <v-icon>notifications_active</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-sheet>
         </v-col>
       </v-row>
     </v-card>
@@ -88,14 +118,30 @@ export default {
       default: () => {}
     }
   },
-  data: () => ({
-    textColor: "#111111"
-  }),
+  data: () => ({}),
   computed: {
+    textColor() {
+      if (this.$vuetify.theme.isDark) return "#FFFFFF";
+      return "#111111";
+    },
     cardColor() {
       if (this.task.missed) return "red";
       if (this.task.checked) return "success";
       return "accent";
+    },
+    iconsize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xl":
+          return "3em";
+        case "lg":
+          return "3em";
+        case "md":
+          return "3em";
+        case "sm":
+          return "3em";
+        default:
+          return "2em";
+      }
     },
     taskMode() {
       switch (this.task.mode) {
@@ -116,13 +162,4 @@ export default {
 };
 </script>
 
-<style>
-.small-main-task {
-  padding: 1em;
-  margin: 1em;
-  margin-bottom: 2.5em;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-</style>
+<style></style>
